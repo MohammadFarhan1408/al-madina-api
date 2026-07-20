d0f99537d7309ba13c21d29f7e3ac1169328065c
MohammadFarhan1408 <farhan14082005@gmail.com>
2026-07-20 22:56:26 +0530

    Add detail endpoint to tags controller and implement getById method with validation

--- src/modules/tags/tags.routes.ts
+++ src/modules/tags/tags.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { tagsController } from './tags.controller';
+import { objectIdParam } from '../../utils/common.schema';
+import { validate } from '../../middlewares';
 import { asyncHandler } from '../../utils/async-handler';
 
 const router = Router();
 
 router.get('/', asyncHandler(tagsController.list));
+router.get('/:id', validate({ params: objectIdParam() }), asyncHandler(tagsController.detail));
 
 export const tagsRoutes = router;
